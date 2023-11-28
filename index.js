485ded35af3393147c9b617f9c028e670bbc9326
TheODDYSEY <mosessabz07@gmail.com>
2023-11-28 23:24:42 +0300

    Adjusted output

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ prompt("Enter a location or a postal code: ", function (location) {
     .get(`${BASE_PATH}?q=${location}&units=imperial&appid=${API_KEY}`)
     .then((response) => {
       const weather = response;
-      const message = `\nCurrent date and time: ${weather.headers.date}\nIt's ${weather.data.main.temp} degrees in ${location}.`;
+      const message = `\nCurrent date and time: ${weather.headers.date}\nIt's ${weather.data.main.temp} farenheit degrees in ${location}.`;
 
       console.log(message);
       process.exit();
